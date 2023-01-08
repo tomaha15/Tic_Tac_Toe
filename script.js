6a6069b1d6f70ede7b08465641d9a7d2659a5507
Nikola <tomaha15@gmail.com>
2023-01-09 04:48:17 +0700

    Remove eventListener from used slot after marking it

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const playerMarks = ['x','o','x','o','x','o','x','o','x','o'];
 const slot = document.querySelectorAll('.slot');
 
 const slots = Array.from(slot);
-const addMark = function(e) {e.target.textContent = markAlternator(playerMarks)};
+const addMark = function(e) {
+    e.target.textContent = markAlternator(playerMarks);
+    e.target.removeEventListener('click', addMark);
+};
 
 
 slots.forEach(function(element){
     element.addEventListener('click', addMark)
 })
 
-let i = 0;
+// Adds x and o alternatively 
+let counter = 0;
 const markAlternator = function (array) {
-    let _mark = array[i];
-    console.log(i)
-    i += 1;
-    console.log(i)
+    let _mark = array[counter];
+    counter += 1;
     return _mark
 }
+
