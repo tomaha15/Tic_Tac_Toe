fd99ef1dde10a587669546909d1274ddf758339e
Nikola <tomaha15@gmail.com>
2023-01-09 04:38:16 +0700

    Add markAlternator fuction

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const playerMarks = ['x','o','x','o','x','o','x','o','x','o'];
 const slot = document.querySelectorAll('.slot');
 
 const slots = Array.from(slot);
-const addMark = function(e) {e.target.textContent = 't'};
+const addMark = function(e) {e.target.textContent = markAlternator(playerMarks)};
 
 
 slots.forEach(function(element){
     element.addEventListener('click', addMark)
 })
 
-
+let i = 0;
+const markAlternator = function (array) {
+    let _mark = array[i];
+    console.log(i)
+    i += 1;
+    console.log(i)
+    return _mark
+}
