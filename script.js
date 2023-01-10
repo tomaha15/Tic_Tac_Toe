d2401fdfb2a09a135ddb1b849d3b9c3beb570a6b
Nikola <tomaha15@gmail.com>
2023-01-10 15:26:04 +0700

    Add checkForWin logic

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const slot = document.querySelectorAll('.slot');
 const slots = Array.from(slot);
 const resetButton = document.querySelector('.reset');
 
-let gameBoardArray = new Array(9);
+let gameBoardArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 
 
 const addMark = function(e) {
@@ -24,6 +24,23 @@ const addMark = function(e) {
     }
 
     arrayWriter(currentSlotNumber);
+
+    //check win condition
+
+    const checkForWin = function (array){
+        if (array[1] === array[0] && array[1] === array[2]) {alert('u win bro')}
+        else if (array[4] === array[3] && array[4] === array[5]) {alert('u win bro')}
+        else if (array[7] === array[6] && array[7] === array[8]) {alert('u win bro')}
+        else if (array[3] === array[0] && array[3] === array[6]) {alert('u win bro')}
+        else if (array[4] === array[1] && array[4] === array[7]) {alert('u win bro')}
+        else if (array[5] === array[2] && array[5] === array[8]) {alert('u win bro')}
+        else if (array[4] === array[0] && array[4] === array[8]) {alert('u win bro')}
+        else if (array[4] === array[6] && array[4] === array[2]) {alert('u win bro')};
+
+    }
+
+    checkForWin(gameBoardArray);
+
 };
 
 
@@ -36,7 +53,7 @@ const resetLogic = function (){
     slots.forEach(element => {element.textContent = ''});
     initialListenerState();
     counter = 0;
-
+    gameBoardArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 }
 
 // Adds x and o alternatively 
@@ -48,3 +65,5 @@ const markAlternator = function (array) {
 }
 
 resetButton.addEventListener('click', resetLogic);
+
+
